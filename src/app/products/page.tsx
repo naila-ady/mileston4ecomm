38b4df99d88e0fe6be5dcb07c2d042b909ef1f90
naila-ady <164941279+naila-ady@users.noreply.github.com>
2025-02-06 21:43:12 +0500

    .

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,7 +1,7 @@
 
 
 import React from 'react';
-import { client } from "../../../sanity-migration/sanityClient";
+import { client } from "../../../sanity-migration/dist/sanityClient";
 import Image from 'next/image';
 import { urlFor } from '@/sanity/lib/image';
 
